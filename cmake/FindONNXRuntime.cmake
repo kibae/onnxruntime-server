efc7a694660e94f642a5cde6dea1e5a6f5bf4999
Kibae Shin <kibae.shin@gmail.com>
2023-10-14 04:04:47 +0900

    ci: cmake support pkg-config and cmake

--- cmake/FindONNXRuntime.cmake
+++ cmake/FindONNXRuntime.cmake
@@ -1,9 +1,10 @@
 message(STATUS "Looking for ONNX Runtime")
 
 set(ONNX_RUNTIME_ROOT_DIR ENV ONNX_ROOT /usr/local/onnxruntime /usr /usr/local /opt/homebrew)
+set(ONNX_RUNTIME_INCLUDE_PATHS /usr/local/include/onnxruntime)
 set(MACOS_ONNX_RUNTIME_INCLUDE_PATHS /opt/homebrew/include/onnxruntime /opt/homebrew/include/onnxruntime/core/session /usr/local/include/onnxruntime/core/session)
 
-find_path(ONNX_RUNTIME_INCLUDE_DIRS onnxruntime_cxx_api.h PATHS ${ONNX_RUNTIME_ROOT_DIR} ${MACOS_ONNX_RUNTIME_INCLUDE_PATHS} PATH_SUFFIXES include)
+find_path(ONNX_RUNTIME_INCLUDE_DIRS onnxruntime_cxx_api.h PATHS ${ONNX_RUNTIME_ROOT_DIR} ${ONNX_RUNTIME_INCLUDE_PATHS} ${MACOS_ONNX_RUNTIME_INCLUDE_PATHS} PATH_SUFFIXES include)
 find_library(ONNX_RUNTIME_LIBRARY onnxruntime PATHS ${ONNX_RUNTIME_ROOT_DIR} PATH_SUFFIXES lib)
 
 find_library(ONNX_RUNTIME_CUDA_LIBRARY onnxruntime_providers_cuda PATHS ${ONNX_RUNTIME_ROOT_DIR} PATH_SUFFIXES lib)
