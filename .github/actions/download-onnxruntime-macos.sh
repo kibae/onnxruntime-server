9fb83cc12381bc51b375382b9196464432e2fa4d
Kibae Shin <kibae.shin@gmail.com>
2024-08-17 16:02:46 +0900

    ci: macos

---
+++ .github/actions/download-onnxruntime-macos.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+cd "$(dirname "$0")" || exit
+
+echo
+echo "Select onnxruntime version to download:"
+RAW_LIST=$(curl -s -H "Accept: application/vnd.github+json" \
+  -H "X-GitHub-Api-Version: 2022-11-28" \
+  https://api.github.com/repos/microsoft/onnxruntime/releases/latest \
+  | grep browser_download_url \
+  | grep -E "onnxruntime-osx-universal([-.0-9]+).tgz" \
+  | awk '{print $2}' \
+  | tr -d '"')
+
+item=${RAW_LIST[0]}
+
+FILENAME=$(basename "$item")
+
+echo
+echo "Downloading $item"
+echo
+
+wget -q "$item"
+
+sudo mkdir -p /usr/local/onnxruntime
+sudo tar vzxf "$FILENAME" -C /usr/local/onnxruntime --strip-components=1
+
+rm -f "$FILENAME"
+
+echo
+echo "Done"
+echo
